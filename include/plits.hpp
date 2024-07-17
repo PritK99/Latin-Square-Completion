8e3b411844980600fd6628de3f0e336cac3264d9
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-07-17 07:01:03 +0530

    Complete implementation for PLITS

--- include/plits.hpp
+++ include/plits.hpp
@@ -15,64 +15,96 @@ using namespace std;
 
 class Compare;
 
+/**
+ * @brief Searches for an element in a vector.
+ * 
+ * @tparam T Type of elements in the vector.
+ * @param v The vector to search in.
+ * @param e The element to find.
+ * @return true if the element is found, false otherwise.
+ */
 template <typename T>
 bool find(vector<T>& v, T e) {
     for (auto& It: v) {
-        if (It == e) return true;
+        if (It == e) return true; // Check if the element exists
     }
-    return false;
+    return false; // Element not found
 }
 
+/**
+ * @brief Represents a graph using an adjacency list.
+ */
 class Graph {
-    public:
-    int n;
-    map<pair<int, int>, vector<pair<int, int>>> adj_list;
+public:
+    int n; // Number of vertices
+    map<pair<int, int>, vector<pair<int, int>>> adj_list; // Adjacency list
 
+    /**
+     * @brief Adds an edge between two vertices.
+     * 
+     * @param n1 The first vertex.
+     * @param n2 The second vertex.
+     */
     void add_edge(pair<int, int> n1, pair<int, int> n2) {
-        adj_list[n1].push_back(n2);
-        adj_list[n2].push_back(n1);
+        adj_list[n1].push_back(n2); // Add n2 to n1's adjacency list
+        adj_list[n2].push_back(n1); // Add n1 to n2's adjacency list
     }
 
+    /**
+     * @brief Removes a vertex and its associated edges from the graph.
+     * 
+     * @param i The vertex to remove.
+     */
     void remove_vertex(pair<int, int> i) {
         n--;
-        adj_list.erase(i);
+        adj_list.erase(i); // Remove the vertex
         for (auto& x: adj_list) {
-            for (int j=0; j<x.second.size(); j++) {
+            for (int j = 0; j < x.second.size(); j++) {
                 if (x.second[j] == i) x.second.erase(x.second.begin() + j);
             }
         }
     }
 
+    /**
+     * @brief Prints the graph's adjacency list.
+     */
     void print_graph() {
         for (auto& x: adj_list) {
-            cout << "(" << x.first.first << ", " << x.first.second << ")"<< ": ";
+            cout << "(" << x.first.first << ", " << x.first.second << ")" << ": ";
             for (auto y: x.second) {
                 cout << "(" << y.first << ", " << y.second << ")" << " ";
             }
-            cout << "\r\n";
+            cout << "\r\n"; // New line after each vertex
         }
     }
-}; 
+};
 
+/**
+ * @brief Represents a Latin square coloring problem using graph coloring.
+ */
 class LSC : public Graph {
-    public:
-    // Latin square
-    vector<vector<int>> square;
-    // Domain set
-    map<pair<int, int>, set<int>> D; 
-    // Color classes
-    map<int, set<pair<int, int>>> V; 
-    // No. of cells impossible to fill (D(u) = {0})
-    int l = 0;
-    // Empty cells (candidates for color filling)
+public:
+    vector<vector<int>> square; // Latin square representation
+    map<pair<int, int>, set<int>> D; // Domain set for each cell
+    map<int, set<pair<int, int>>> V; // Color classes
+    int l = 0; // Number of cells impossible to fill (D(u) = {0})
     vector<pair<int, int>> Cand_set;
 
+    /**
+     * @brief Default constructor.
+     */
     LSC() {
         ;
     }
 
+    /**
+     * @brief Retrieves the color of a specified vertex.
+     * 
+     * @param vertex The vertex to retrieve the color for.
+     * @return The color of the vertex, or 0 if not found.
+     */
     int get_color(pair<int, int> vertex) {
-        for (int i=0; i<=square.size(); i++) {
+        for (int i = 0; i <= square.size(); i++) {
             if (V[i].find(vertex) != V[i].end()) {
                 return i;
             }
@@ -80,10 +112,11 @@ class LSC : public Graph {
         return 0;
     }
 
+    /**
+     * @brief Initializes the color assignment for empty cells.
+     */
     void init() {
-        // Randomly assign color sets to remaining candidates
         shuffle(Cand_set.begin(), Cand_set.end(), default_random_engine(time(0)));
-        // srand(time(0));
         for (auto& It: Cand_set) {
             if (D[It].size() != 0) {
                 int r = rand() % D[It].size();
@@ -97,25 +130,36 @@ class LSC : public Graph {
         }
     }
 
+    /**
+     * @brief Generates a population of LSC instances based on the current state.
+     * 
+     * @param pop_size The size of the population to generate.
+     * @return A vector containing the generated LSC instances.
+     */
     vector<LSC> gen_population(int pop_size) {
-        vector<LSC> res;
+        vector<LSC> res; // Resulting population
         #pragma omp parallel for shared(res)
-        for (int i=0; i<pop_size; i++) {
+        for (int i = 0; i < pop_size; i++) {
             LSC temp;
-            temp.square = square;
+            temp.square = square; // Copy current state
             temp.D = D;
             temp.V = V;
             temp.n = n;
             temp.l = l;
             temp.Cand_set = Cand_set;
             temp.adj_list = adj_list;
-            temp.init();
+            temp.init(); // Initialize the new instance
             #pragma omp critical
-            res.push_back(temp);
+            res.push_back(temp); // Add to the population
         }
         return res;
     }
 
+    /**
+     * @brief Overloads the assignment operator for LSC.
+     * 
+     * @param L The LSC instance to copy from.
+     */
     void operator=(const LSC& L) {
         square = L.square;
         D = L.D;
@@ -126,102 +170,131 @@ class LSC : public Graph {
         adj_list = L.adj_list;
     }
 
+    /**
+     * @brief Constructs an LSC from a partial Latin square.
+     * 
+     * @param partial The initial partial Latin square.
+     */
     LSC(vector<vector<int>> partial) {
         int x = partial.size();
         n = x * x;
         square = partial;
-        for (int i=0; i<x; i++) {
-            for (int j=0; j<x; j++) {
-                for (int k=j+1; k<x; k++) {
+
+        // Build adjacency list and initialize candidate set
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                for (int k = j + 1; k < x; k++) {
                     add_edge({i, j}, {i, k});
                     add_edge({j, i}, {k, i});
                 }
                 if (square[i][j] == 0) {
-                    Cand_set.push_back({i, j});
-                    for (int k=0; k<=x; k++) {
-                        D[{i, j}].insert(k);
+                    Cand_set.push_back({i, j}); // Add empty cells to candidate set
+                    for (int k = 0; k <= x; k++) {
+                        D[{i, j}].insert(k); // Initialize domain set for empty cells
                     }
-                }
-            }
-        }
-        
-        // Conf for already filled cells
-        for (int i=0; i<x; i++) {
-            for (int j=0; j<x; j++) {
-                if (square[i][j] != 0) {
-                    for (auto It : adj_list[{i, j}]) {
-                        D[It].erase(square[i][j]);
-                    }
-                    V[square[i][j]].insert({i, j});
-                    // remove_vertex({i, j});
                 }
             }
         }
 
-        // Reduction of domain set using smart solving
+        // Configure already filled cells
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (square[i][j] != 0) {
+                    for (auto It : adj_list[{i, j}]) {
+                        D[It].erase(square[i][j]); // Remove filled color from adjacent cells
+                    }
+                    V[square[i][j]].insert({i, j}); // Assign the filled color
+                }
+            }
+        }
+
+        // Reduce domain set using smart solving
         bool isReducible = true;
         while (isReducible) {
             isReducible = false;
-            for (int i=0; i<x; i++) {
-                for (int j=0; j<x; j++) {
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < x; j++) {
                     // Only one possible color can be assigned
-                    if (D[{i, j}].size() == 1)
-                    {
+                    if (D[{i, j}].size() == 1) {
                         isReducible = true;
-                        int color = *D[{i, j}].begin();
-                        V[color].insert({i, j});
-                        // D[{i, j}].erase(color);
-                        square[i][j] = color;
-                        if (color == 0) l++;
+                        int color = *D[{i, j}].begin(); // Get the only color
+                        V[color].insert({i, j}); // Assign color to the cell
+                        square[i][j] = color; // Update the square
+                        if (color == 0) l++; // Count empty cells
                         else {
                             for (auto It : adj_list[{i, j}]) {
-                                D[It].erase(color);
+                                D[It].erase(color); // Update adjacent cells' domains
                             }
                         }
                         pair<int, int> n1 = {i, j};
-                        // Removing the cell from the candidate set
-                        Cand_set.erase(remove(Cand_set.begin(), Cand_set.end(), n1), Cand_set.end());
-                        remove_vertex({i, j});
+                        Cand_set.erase(remove(Cand_set.begin(), Cand_set.end(), n1), Cand_set.end()); // Remove from candidates
+                        remove_vertex({i, j}); // Remove the vertex
                     }
                 }
             }
         }
-    } 
+    }
 
+    /**
+     * @brief Moves a vertex from one color to another.
+     * 
+     * @param v The vertex to move.
+     * @param color1 The color to move from.
+     * @param color2 The color to move to.
+     */
     void Move(pair<int, int> v, int color1, int color2) {
         V[color1].erase(v);
         V[color2].insert(v);
         square[v.first][v.second] = color2;
     }
 
+    /**
+     * @brief Calculates the number of conflicts in the current coloring.
+     * 
+     * @return The total number of conflicts.
+     */
     int CL() {
         int res = 0; 
         for (auto& color_set: V) {
-            if (color_set.first == 0) continue;
+            if (color_set.first == 0) continue; // Skip empty color
             auto It = color_set.second.begin();
             while (It != color_set.second.end()) {
                 auto next_It = It;
                 next_It++;
-                while(next_It != color_set.second.end()) {
+                while (next_It != color_set.second.end()) {
                     if (find(adj_list[*It], *next_It)) {
-                        res++;
+                        res++; // Count conflicts
                     }
                     next_It++;
                 }
                 It++;
             }
         }
-        return res;
+        return res; // Return total conflicts
     }
 
+    /**
+     * @brief Calculates the number of unfilled cells (color 0).
+     * 
+     * @return The count of unfilled cells.
+     */
     int f() {
         return V[0].size();
     }
 
+    /**
+     * @brief Calculates the fitness function value.
+     * 
+     * @param phi A weight parameter for conflict penalty.
+     * @return The calculated fitness value.
+     */
     int F(float phi) {
         return f() + phi * CL();
     }
 
+    /**
+     * @brief Prints the domain set for each cell.
+     */
     void print_domains() {
         int x = square.size();
         for (auto& It: adj_list) {
@@ -231,66 +304,82 @@ class LSC : public Graph {
             }
             cout << "\n";
         }
-    } 
+    }
 
+    /**
+     * @brief Prints the color sets assigned to cells.
+     */
     void print_color_set() {
-        for (int i=0; i<=square.size(); i++) {
+        for (int i = 0; i <= square.size(); i++) {
             cout << i << ": ";
             for (const pair<int, int> i: V[i]) {
                 cout << i.first << "," << i.second << " ";
             }
             cout << "\n";
         }
-    }   
+    }
 
+    /**
+     * @brief Prints the current state of the Latin square.
+     */
     void printSquare() {
         int x = square.size();
-        for (int i=0; i < x; i++) {
-            for (int j=0; j < x; j++) {
-                cout << square[i][j] << " " ;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                cout << square[i][j] << " ";
             }
             cout << "\n";
         }
     }
 
+    /**
+     * @brief Checks if the current state is a goal state (no conflicts).
+     * 
+     * @return true if the goal state is reached, false otherwise.
+     */
     bool GoalTest() {
-        if (CL() == 0) {
-            return true;
-        }
-        return false;
+        return CL() == 0; // Goal if no conflicts
     }
 
     void MoveGen(priority_queue<pair<LSC, int>, vector<pair<LSC, int>>, Compare>& Q, map<vector<vector<int>>, int>& visited);
     void MoveGen(int phi, int tt, priority_queue<pair<LSC, int>, vector<pair<LSC, int>>, Compare>& Q, map<vector<vector<int>>, int>& tabu);
-
 };
 
+/**
+ * @brief Comparator for the priority queue to order LSC instances by their cost.
+ */
 class Compare {
-    public:
-        bool operator() (const pair<LSC, int>& a, const pair<LSC, int>& b) {
-            return (a.second > b.second);
-        }
+public:
+    bool operator() (const pair<LSC, int>& a, const pair<LSC, int>& b) {
+        return (a.second > b.second); // Higher cost has lower priority
+    }
 };
 
+/**
+ * @brief Generates neighbor states by moving colors and adding them to the priority queue.
+ * 
+ * @param Q The priority queue for storing neighbors.
+ * @param visited The map of visited states to avoid duplicates.
+ */
 void LSC::MoveGen(priority_queue<pair<LSC, int>, vector<pair<LSC, int>>, Compare>& Q, map<vector<vector<int>>, int>& visited) {
     vector<int> colors;
-    for (int i=1; i<=square.size(); i++) {
-        colors.push_back(i);
+    for (int i = 1; i <= square.size(); i++) {
+        colors.push_back(i); // List of possible colors
     }
     for (auto& color_set: V) {
         for (auto& cell: color_set.second) {
             for (auto& color_next: colors) {
-                if (color_next != color_set.first) {
+                if (color_next != color_set.first) { // Avoid moving to the same color
                     LSC neigh;
-                    neigh.n = n;
+                    neigh.n = n; // Copy state
                     neigh.adj_list = adj_list;
                     neigh.square = square;
                     neigh.V = V;
                     neigh.D = D;
                     neigh.l = l;
-                    neigh.Move(cell, color_set.first, color_next);
+                    neigh.Move(cell, color_set.first, color_next); // Generate neighbor
                     if (visited[neigh.square] != 1) {
-                        Q.push({neigh, CL() - neigh.CL()});
+                        Q.push({neigh, CL() - neigh.CL()}); // Add to priority queue if not visited
                     }
                 }
             }
@@ -298,27 +387,35 @@ void LSC::MoveGen(priority_queue<pair<LSC, int>, vector<pair<LSC, int>>, Compare
     }
 }
 
+/**
+ * @brief Generates neighbor states considering tabu search to avoid revisiting states.
+ * 
+ * @param phi Penalty weight.
+ * @param tt Tabu tenure.
+ * @param Q The priority queue for storing neighbors.
+ * @param tabu The map of tabu states.
+ */
 void LSC::MoveGen(int phi, int tt, priority_queue<pair<LSC, int>, vector<pair<LSC, int>>, Compare>& Q, map<vector<vector<int>>, int>& tabu) {
     vector<int> colors;
-    for (int i=1; i<=square.size(); i++) {
-        colors.push_back(i);
+    for (int i = 1; i <= square.size(); i++) {
+        colors.push_back(i); // List of possible colors
     }
     for (auto& color_set: V) {
         for (auto& cell: color_set.second) {
             for (auto& color_next: colors) {
-                if (color_next != color_set.first) {
+                if (color_next != color_set.first) { // Avoid moving to the same color
                     LSC neigh;
-                    neigh.n = n;
+                    neigh.n = n; // Copy state
                     neigh.adj_list = adj_list;
                     neigh.square = square;
                     neigh.V = V;
                     neigh.D = D;
                     neigh.l = l;
-                    neigh.Move(cell, color_set.first, color_next);
-                    if (tabu[neigh.square] == 0) {
-                        Q.push({neigh, neigh.F(phi)});
+                    neigh.Move(cell, color_set.first, color_next); // Generate neighbor
+                    if (tabu[neigh.square] == 0) { // Check if not in tabu list
+                        Q.push({neigh, neigh.F(phi)}); // Add to priority queue with fitness
                     }
-                    tabu[neigh.square] = tt;
+                    tabu[neigh.square] = tt; // Update tabu tenure
                 }
             }
         }
--- src/plits_par.cpp
+++ src/plits_par.cpp
@@ -3,104 +3,147 @@
 
 using namespace std;
 
-LSC PLITS(LSC S) {
+/**
+ * @brief Executes the PLITS algorithm to optimize the given LSC.
+ *
+ * @param S The initial LSC structure to optimize.
+ * @return LSC The best solution found after optimization.
+ */
+LSC PLITS(LSC S)
+{
     priority_queue<pair<LSC, int>, vector<pair<LSC, int>>, Compare> Open;
     srand(time(0));
     int T = (rand() % 9) + 0.6 * (S.V[0].size() + S.CL());
     map<vector<vector<int>>, int> tabu_list;
-    // map<vector<vector<int>>, int> Close;
     LSC final = S;
     float phi = 0.6;
+
     Open.push({S, S.F(phi)});
-    // cout << final.CL() << "," << final.F(phi) << "\n";
-    for (int i=0; i<(100 * S.V.size()) && !Open.empty(); i++) {
+
+    // First phase of the PLITS algorithm
+    for (int i = 0; i < (100 * S.V.size()) && !Open.empty(); i++)
+    {
         auto curr = Open.top();
-        // curr.first.printSquare(); 
-        // cout << curr.first.F(phi) << "\n";
         Open.pop();
         curr.first.MoveGen(phi, T, Open, tabu_list);
-        for (auto& It: tabu_list) {
+
+        // Decrease the tabu tenure for all entries in the tabu list
+        for (auto &It : tabu_list)
+        {
             It.second--;
         }
-        if (final.F(phi) > curr.first.F(phi)) {
+
+        if (final.F(phi) > curr.first.F(phi))
+        {
             final = curr.first;
         }
     }
+
     phi = S.V.size();
-    // final.printSquare();
-    // cout << final.CL() << "\n";
     Open = priority_queue<pair<LSC, int>, vector<pair<LSC, int>>, Compare>();
     tabu_list.clear();
     Open.push({S, S.F(phi)});
-    for (int i=0; i<(10 * S.V.size()) && !Open.empty(); i++) {
+
+    // Second phase of the PLITS algorithm
+    for (int i = 0; i < (10 * S.V.size()) && !Open.empty(); i++)
+    {
         auto curr = Open.top();
-        // curr.first.printSquare(); 
-        // cout << curr.first.F(phi) << "\n";
         Open.pop();
         curr.first.MoveGen(phi, T, Open, tabu_list);
-        for (auto& It: tabu_list) {
+
+        for (auto &It : tabu_list)
+        {
             It.second--;
         }
-        if (final.F(phi) > curr.first.F(phi)) {
+
+        if (final.F(phi) > curr.first.F(phi))
+        {
             final = curr.first;
         }
     }
-    // final.printSquare();
-    // cout << final.CL() << "\n";
+
     return final;
 }
 
-int dist(LSC A, LSC B) {
+/**
+ * @brief Calculates the distance between two LSC objects.
+ *
+ * @param A The first LSC object.
+ * @param B The second LSC object.
+ * @return int The distance between A and B, or -1 if sizes differ.
+ */
+int dist(LSC A, LSC B)
+{
     int res = 0;
-    if (A.square.size() != B.square.size()) return -1;
+    if (A.square.size() != B.square.size())
+        return -1;
     int x = A.square.size();
-    for (int i=0; i<x; i++) {
-        for (int j=0; j<x; j++) {
-            if (A.get_color({i, j}) != B.get_color({i, j})) res++;
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            if (A.get_color({i, j}) != B.get_color({i, j}))
+                res++;
         }
     }
     return res;
 }
 
-vector<vector<int>> dist_matrix_calc(vector<LSC>& init_pop, vector<LSC>& improved) {
+/**
+ * @brief Computes the distance matrix between two populations of LSC objects.
+ *
+ * @param init_pop The initial population of LSC objects.
+ * @param improved The improved population of LSC objects.
+ * @return vector<vector<int>> A matrix containing distances between populations.
+ */
+vector<vector<int>> dist_matrix_calc(vector<LSC> &init_pop, vector<LSC> &improved)
+{
     int n = init_pop.size();
     vector<vector<int>> res(n, vector<int>(n, 0));
-    for(int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             res[i][j] = dist(init_pop[i], improved[j]);
         }
     }
     return res;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     int num_threads = 16; // Default number of threads
-    if (argc > 1) 
+    if (argc > 1)
     {
         num_threads = stoi(argv[1]);
     }
     omp_set_num_threads(num_threads);
 
+    cout << "Number of threads: " << omp_get_max_threads() << endl;
+
     LSC test(
-        {{3, 0, 0, 0, 0},
-         {0, 0, 0, 2, 0},
-         {0, 0, 1, 0, 0},
-         {0, 0, 0, 0, 0},
-         {0, 4, 0, 1, 5},
-        }
-    );
+        {
+            {3, 0, 0, 0, 0},
+            {0, 0, 0, 2, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 4, 0, 1, 5},
+        });
 
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
     vector<LSC> res = test.gen_population(5);
-    for (LSC& i: res) {
+    for (LSC &i : res)       // Printing the intermediate states
+    {
         i.printSquare();
         cout << "\n";
     }
     vector<LSC> imp;
-    #pragma omp parallel for
-    for (LSC& i: res) {
+#pragma omp parallel for
+    for (LSC &i : res)
+    {
         LSC temp = PLITS(i);
         imp.push_back(temp);
     }
@@ -108,47 +151,55 @@ int main(int argc, char* argv[]) {
 
     // P_all
     vector<pair<LSC, pair<int, int>>> P_all;
-    for (int i=0; i<res.size(); i++) {
+    for (int i = 0; i < res.size(); i++)
+    {
         P_all.push_back({res[i], {0, i}});
     }
 
-    for (int i=0; i<imp.size(); i++) {
+    for (int i = 0; i < imp.size(); i++)
+    {
         P_all.push_back({imp[i], {1, i}});
     }
 
     vector<pair<LSC, pair<int, int>>> new_pop;
 
-    sort(P_all.begin(), P_all.end(), [&](pair<LSC, pair<int, int>>& a, pair<LSC, pair<int, int>>& b){return a.first.CL() < b.first.CL();});
+    sort(P_all.begin(), P_all.end(), [&](pair<LSC, pair<int, int>> &a, pair<LSC, pair<int, int>> &b) { return a.first.CL() < b.first.CL(); });
 
     // New pop
     new_pop.push_back(P_all[0]);
     P_all.erase(P_all.begin());
 
-    while (new_pop.size() < 5) {
+    while (new_pop.size() < 5)
+    {
         auto x = P_all[0];
         int dist = INT32_MAX;
-        for (auto i: new_pop) {
-            if (x.second.first == 0 && i.second.first == 0) dist = min(dist, dist_mat_int[i.second.second][x.second.second]);
-            else {
+        for (auto i : new_pop)
+        {
+            if (x.second.first == 0 && i.second.first == 0)
+                dist = min(dist, dist_mat_int[i.second.second][x.second.second]);
+            else
+            {
                 dist = min(dist, dist_mat[i.second.second][x.second.second]);
             }
         }
-        if (dist > 5 / 2) {
+        if (dist > 5 / 2)
+        {
             new_pop.push_back(x);
             P_all.erase(P_all.begin());
-        }   
+        }
     }
 
     cout << "Solutions: \n";
 
-    for (auto& i: new_pop) {
+    for (auto &i : new_pop)
+    {
         i.first.printSquare();
         cout << "\n";
     }
 
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
- 
+
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
     return 0;
 }
